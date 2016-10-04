89596baded0d1bbca12e266dbbd1f3f476170a08
diego1soler <dsoler1998@gmail.com>
2016-10-04 16:59:41 -0600

    version final

--- source/oez.s
+++ source/oez.s
@@ -7,6 +7,29 @@
  .balign 4
  .global main
 main:
+
+
+    /*Obtiene la dirección del GPIO de la raspberry*/
+	bl GetGpioAddress
+	ldr r1,=myloc
+	str r0,[r1]
+	
+	/*GPIO para escritura puerto 16 (Avanza izquierda)*/
+	mov r0,#16
+	mov r1,#0
+	bl SetGpioFunction
+	
+    /*GPIO para escritura puerto 20 (Avanza derecha)*/
+	mov r0,#20
+	mov r1,#0
+	bl SetGpioFunction
+
+    /*GPIO para escritura puerto 26 (Seleccionar disparar)*/
+	mov r0,#26
+	mov r1,#0
+	bl SetGpioFunction
+	
+	
         ldr r0, =menuWidth
         ldr r0, [r0]
         ldr r1, =menuHeight
@@ -14,13 +37,65 @@ main:
         ldr r2, =menu
         bl draw
 
+		
+	seleccion:
+	
         ldr r0, =cursorWidth
         ldr r0, [r0]
         ldr r1, =cursorHeight
         ldr r1, [r1]
         ldr r2, =cursor
-        bl draw
+		ldr r3,=posY
+		ldr r3,[r3]
+        bl drawCursor
+		
+		
+	
+		
+	loop:
 
+	
+	/*Verifica si los botones han sido presionados*/
+		
+	bl boton
+	
+	cmp  r1,#1
+	bleq revisarUp
+	beq seleccion
+	
+	cmp r1,#2
+	bleq revisarDown
+	beq seleccion
+	
+	cmp r1,#3
+	bleq seleccionar
+	
+	b loop
+
+
+	
+	
+	
+		
 end:    mov r7,#1
         swi 0
+		
+.data
+.global myloc
+.global cursorPos
+.global y1
+.global y3
+.global posY
 
+myloc:
+	.word 0x3F200000
+posY:
+	.word 360
+cursorPos:
+	.word 0
+y1:
+	.word 504
+y2:
+	.word 590
+y3: 
+	.word 666
--- source/subrutinas.s
+++ source/subrutinas.s
@@ -76,9 +76,7 @@ loopy:
         add r9, #1
         b loopy
 
-end:    
-        pop {r4-r9}     /* Standard ABI */
-        pop {pc}
+
 
 .global drawSprite
 
@@ -155,22 +153,27 @@ loopy_sprite:
         b loopy_sprite
 
 
-.global drawShip
+.global drawCursor
 
 /*
 * Esta subrutina dibuja una imagen en 8 bits, obviando el fondo.
-* PARAMETROS: r0 = Width x, r1 = Height y, r2 = direccion de la matriz de pixeles a dibujar 
+* PARAMETROS: r0 = Width x, r1 = Height y, r2 = direccion de la matriz de pixeles a dibujar, r3 = la posicion y donde inicia a pintar
 */
 
 
-drawShip:
+drawCursor:
         push {lr}
         push {r4-r9}    /* Standrad ABI */  
 
         /* Se mueven los parametros a nuevos registros */
         mov r4, r0 
         mov r5, r1  
-        mov r6, r2       
+        mov r6, r2  
+		mov r11, r3
+		ldr r10,=y_cursor
+		str r11,[r10]
+		ldr r10,=cursorPos
+		str r11,[r10]
         
         /********* Reset Y **********/
         ldr r2, =y
@@ -185,7 +188,7 @@ drawShip:
                    
         mov r9, #0      /* Contador */
 
-loopy_ship:
+loopy_spritey:
         /******** Reset X ********/
         mov r8, #0
         ldr r7, =x_checkpoint    
@@ -194,15 +197,16 @@ loopy_ship:
         str r7, [r1]
       
         cmp r9, r5
+		ldrgt r0,=cursorPos
         bgt end
-        loopx_ship:  
+        loopx_cursor:  
                 ldr r0,=pixelAddr
                 ldr r0,[r0]
 
                 ldr r1,=x
                 ldr r1,[r1]
                 
-                ldr r2,=y_ship
+                ldr r2,=y_cursor
                 ldr r2,[r2]
 
                 
@@ -212,24 +216,128 @@ loopy_ship:
                 blne pixel
 
                 /* Barrido en X */
-                ldr r1,=x_ship
+                ldr r1,=x
                 ldr r7,[r1]
                 add r7,#1
                 str r7,[r1]
 
                 add r8, #1
                 cmp r8, r4
-                blt loopx_ship
+                blt loopx_cursor
 
         /* Barrido en Y */
-        ldr r2, =y_ship
+        ldr r2, =y_cursor
         ldr r7, [r2]
         add r7, #1
         str r7, [r2]
         add r9, #1
-        b loopy_ship
+       	b loopy_cursor
 		
 
+/*
+* SUBRUTINAS PARA LEER EL ESTADO DE LOS PUSH BUTTON
+* PARAMETROS: NINGUNO
+* SALIDAS: 1 en r0 si el boton fue presionado, y 0 en r0 si no se presiona
+*/
+
+.global boton		
+boton:
+	
+	push {lr}
+	
+    mov r0,#16
+	bl GetGpio
+	cmp r0,#1
+	moveq r1,#1
+
+    mov r0,#20
+	bl GetGpio
+	cmp r0,#1
+	moveq r1,#2
+	
+    mov r0,#26
+	bl GetGpio
+	cmp r0,#1
+	moveq r1,#3
+	
+	pop {pc}
+
+
+
+.global revisarUp	
+revisarUp:
+
+	push {lr}
+	ldr r0,=cursorPos
+	ldr r0,[r0]
+	
+	ldr r2,=posY
+	
+	ldr r1,=y1
+	ldr r1,[r1]
+	
+	cmp r0,r1
+	moveq r0,r3
+	streq r0,[r3]
+	moveq pc,lr
+	
+	sub r0,r0,#86
+	str r0,[r2]
+	mov pc,lr
+
+	
+	
+.global revisarDown
+revisarDown:
+
+	push {lr}
+	ldr r0,=cursorPos
+	ldr r0,[r0]
+	
+	ldr r2,=posY
+	
+	ldr r1,=y3
+	ldr r1,[r1]
+	
+	cmp r0,r1
+	moveq r0,r3
+	streq r0,[r3]
+	moveq pc,lr
+	
+	add r0,r0,#86
+	str r0,[r2]
+	mov pc,lr
+
+.global seleccionar
+
+seleccionar:
+	
+	push {lr}
+	ldr r0,=cursorPos
+	ldr r0,[r0]
+	
+	ldr r1,=y1
+	ldr r1,[r1]
+	
+	cmp r0,r1
+	/*bleq jugar*/
+	
+	ldr r1,=y2
+	ldr r1,[r1]
+	
+	cmp r0,r1
+	/*bleq instrucciones*/
+	
+	ldr r1,=y3
+	ldr r1,[r1]
+	
+	cmp r0,r1
+	/*bleq salir*/
+	
+
+end:    
+        pop {r4-r9}     /* Standard ABI */
+        pop {pc}
 
 		
 .data
@@ -245,9 +353,12 @@ x_checkpoint:
 y: 
 	.word 0
 y_sprite: 
-	.word 600
-y_ship:  
+	.word 400
+y_cursor:  
 	.word 0
-x_ship:  
-	.word 0
+x_cursor:
+	.word 360
+x_cursor1:
+	.word 530
+
 
